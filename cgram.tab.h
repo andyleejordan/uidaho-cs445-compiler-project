be1bcc9a3d81e3f40200fd31ff6bc001e480a040
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-09-02 20:09:48 -0700

    Replacing cgram with ytab contents

--- cgram.tab.h
+++ cgram.tab.h
@@ -1,162 +1,92 @@
+#define ELIPSIS 257
+#define AUTO 258
 
-/* A Bison parser, made by GNU Bison 2.4.1.  */
+/* keywords */
+#define BREAK 259
+#define CASE 260
+#define CHAR 261
+#define CONST 262
+#define CONTINUE 263
+#define DEFAULT 264
+#define DO 265
+#define DOUBLE 266
+#define ELSE 267
+#define ENUM 268
+#define EXTERN 269
+#define FLOAT 270
+#define FOR 271
+#define GOTO 272
+#define IF 273
+#define INT 274
+#define LONG 275
+#define REGISTER 276
+#define RETURN 277
+#define SHORT 278
+#define SIGNED 279
+#define SIZEOF 280
+#define STATIC 281
+#define STRUCT 282
+#define SWITCH 283
+#define TYPEDEF 284
+#define UNION 285
+#define UNSIGNED 286
+#define VOID 287
+#define VOLATILE 288
+#define WHILE 289
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
-   
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+/* constants */
+#define ICON 290
+#define CCON 291
+#define FCON 292
+#define STRING 293
 
-/* As a special exception, you may create a larger work that contains
-   part or all of the Bison parser skeleton and distribute that work
-   under terms of your choice, so long as that work isn't itself a
-   parser generator using the skeleton or a modified version thereof
-   as a parser skeleton.  Alternatively, if you modify or redistribute
-   the parser skeleton itself, you may (at your option) remove this
-   special exception, which will cause the skeleton and the resulting
-   Bison output files to be licensed under the GNU General Public
-   License without this special exception.
-   
-   This special exception was added by the Free Software Foundation in
-   version 2.2 of Bison.  */
+/* operators */
+#define SRASN 294
+#define SLASN 295
+#define PLASN 296
+#define MIASN 297
+#define MUASN 298
+#define DIASN 299
+#define MOASN 300
+#define ANASN 301
+#define ERASN 302
+#define ORASN 303
+#define SHR 304
+#define SHL 305
+#define INCOP 306
+#define DECOP 307
+#define FOLLOW 308
+#define ANDAND 309
+#define OROR 310
+#define LE 311
+#define GE 312
+#define EQ 313
+#define NE 314
+#define SM 315
+#define LC 316
+#define RC 317
+#define CM 318
+#define COLON 319
+#define ASN 320
+#define LP 321
+#define RP 322
+#define LB 323
+#define RB 324
+#define DOT 325
+#define AND 326
+#define BANG 327
+#define TILDE 328
+#define MINUS 329
+#define PLUS 330
+#define MUL 331
+#define DIV 332
+#define MOD 333
+#define LT 334
+#define GT 335
+#define ER 336
+#define OR 337
+#define QUEST 338
 
-
-/* Tokens.  */
-#ifndef YYTOKENTYPE
-# define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     BAD_TOKEN = 258,
-     ICON = 259,
-     CCON = 260,
-     FCON = 261,
-     ENUMERATION_CONSTANT = 262,
-     IDENTIFIER = 263,
-     STRING = 264,
-     SIZEOF = 265,
-     INCOP = 266,
-     DECOP = 267,
-     SHL = 268,
-     SHR = 269,
-     LE = 270,
-     GE = 271,
-     EQ = 272,
-     NE = 273,
-     ANDAND = 274,
-     OROR = 275,
-     MUASN = 276,
-     DIASN = 277,
-     MOASN = 278,
-     PLASN = 279,
-     ASN = 280,
-     MIASN = 281,
-     SLASN = 282,
-     SRASN = 283,
-     ANASN = 284,
-     ERASN = 285,
-     ORASN = 286,
-     TYPEDEF_NAME = 287,
-     CM = 288,
-     SM = 289,
-     LT = 290,
-     GT = 291,
-     PLUS = 292,
-     MINUS = 293,
-     MUL = 294,
-     DIV = 295,
-     MOD = 296,
-     LP = 297,
-     RP = 298,
-     LB = 299,
-     RB = 300,
-     LC = 301,
-     RC = 302,
-     COLON = 303,
-     QUEST = 304,
-     AND = 305,
-     OR = 306,
-     ER = 307,
-     NOT = 308,
-     FOLLOW = 309,
-     BANG = 310,
-     DOT = 311,
-     SP = 312,
-     DTAB = 313,
-     ITAB = 314,
-     EOL = 315,
-     TAB = 316,
-     ECNT = 317,
-     TYPEDEF = 318,
-     EXTERN = 319,
-     STATIC = 320,
-     AUTO = 321,
-     REGISTER = 322,
-     CHAR = 323,
-     SHORT = 324,
-     INT = 325,
-     LONG = 326,
-     SIGNED = 327,
-     UNSIGNED = 328,
-     FLOAT = 329,
-     DOUBLE = 330,
-     CONST = 331,
-     VOLATILE = 332,
-     VOID = 333,
-     STRUCT = 334,
-     UNION = 335,
-     ENUM = 336,
-     ELIPSIS = 337,
-     CASE = 338,
-     DEFAULT = 339,
-     IF = 340,
-     SWITCH = 341,
-     WHILE = 342,
-     DO = 343,
-     FOR = 344,
-     GOTO = 345,
-     CONTINUE = 346,
-     BREAK = 347,
-     RETURN = 348,
-     THEN = 349,
-     ELSE = 350
-   };
-#endif
-
-
-
-#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-{
-
-/* Line 1676 of yacc.c  */
-#line 19 "cgram.y"
-
-struct token *tokptr;
-struct tree *treeptr;
-
-
-
-/* Line 1676 of yacc.c  */
-#line 154 "cgram.tab.h"
-} YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
-#endif
-
-extern YYSTYPE yylval;
-
-
+#define IDENTIFIER 339
+#define TYPEDEF_NAME 340
+#define BAD_TOKEN 341
