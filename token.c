bee6313bf763b5ddf8e6a57925b9fba82cf10869
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-09-06 17:43:48 -0700

    Refactoring token modifiers into token.c
    - Adding wrapper new_token() to call create_token() with Flex globals
    - Moved token.sval and token.text realloc and appends
    - Includes pop of filename on EOF (related to last commit)

---
+++ token.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "token.h"
+#include "cgram.tab.h"
+
+struct token *token_create(int category, int lineno, char *text, char* filename)
+{
+	struct token *t = malloc(sizeof(*t));
+
+	t->category = category;
+	t->lineno = lineno;
+
+	t->text = calloc(strlen(text)+1, sizeof(char));
+	strcpy(t->text, text);
+
+	t->filename = calloc(strlen(filename)+1, sizeof(char));
+	strcpy(t->filename, filename);
+
+	if (category == ICON)
+		t->ival = atoi(text);
+
+	if (category == SCON)
+		t->sval = calloc(128, sizeof(char));
+
+	return t;
+}
+
+/* reallocate t->sval to append string and terminating null */
+void token_realloc_sval(struct token *t, char *s)
+{
+	size_t size = strlen(t->sval)+strlen(s)+1;
+	t->sval = realloc(t->sval, size);
+}
+
+void token_append_sval_char(struct token *t, char c)
+{
+	size_t len = strlen(t->sval);
+	token_realloc_sval(t, &c);
+	t->sval[len] = c;
+}
+
+void token_append_sval_string(struct token *t, char *s)
+{
+	token_realloc_sval(t, s);
+	strcat(t->sval, s);
+}
+
+/* reallocate yytoken->text to append string and terminating null */
+void token_realloc_text(struct token *t, char *s)
+{
+	size_t size = strlen(t->text)+strlen(s)+1;
+	t->text = realloc(t->text, size);
+}
+
+void token_append_text(struct token *t, char* s)
+{
+	token_realloc_text(t, s);
+	strcat(t->text, s);
+}
--- token.h
+++ token.h
@@ -13,4 +13,16 @@ struct token
 	char *sval;
 };
 
+/* malloc token and assign values */
+struct token *token_create(int category, int lineno, char *text, char* filename);
+
+/* append char literal to yytoken->sval string (to parse escapes) */
+void token_append_sval_char(struct token *t, char c);
+
+/* append string literal to yytoken->sval string (with null) */
+void token_append_sval_string(struct token *t, char *s);
+
+/* append string literal yytoken->text (with null) */
+void token_append_text(struct token *t, char* s);
+
 #endif /* TOKEN_H */
