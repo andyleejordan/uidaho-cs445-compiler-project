7aa89e3c6b6c47b87c2b799cb6457e5204824bf5
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-09-11 16:05:05 -0700

    Adding tests for sval chunk allocation algorithm

--- test/lex/test.c
+++ test/lex/test.c
@@ -42,8 +42,9 @@ int main(int argc, char *argv[])
 		: 2;
 
 	free(tmp);
+
+	/* 512 bytes */
+	char *loremipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tincidunt id velit nec laoreet. Maecenas feugiat cursus finibus. Ut convallis volutpat urna, nec cursus nisl. Nullam malesuada sapien et maximus ultricies. Suspendisse ut justo dui. Nulla bibendum id turpis sed lobortis. Sed vitae turpis vitae arcu pulvinar aliquet. Nam a faucibus mi. Integer rutrum mattis nisl, facilisis pharetra lorem dapibus finibus. Aenean eget lacus non augue faucibus lobortis et a sapien. Duis eleifend maximus cras amet.";
+
+	char *nullsucks = "A string with \0 i.e. escaped null (\0) a couple times";
 }
-
-
-
-
