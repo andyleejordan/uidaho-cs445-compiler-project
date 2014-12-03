560192a1f454a81f6b3ec2db4c5b27d66c47d03f
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-12-03 02:44:36 -0800

    Updating opcodes

--- intermediate.h
+++ intermediate.h
@@ -14,28 +14,29 @@
 
 enum opcode {
 	/* pseudo opcodes */
-	GLOBAL, /* name, address 1 offset, address 2 size */
-	LABEL,  /* name, address 1 label */
-	PROC,   /* name, */
+	GLOBAL, /* name, address in GLOBAL_R */
+	PROC,   /* name, address in GLOBAL_R */
+	LOCAL,  /* name, address in LOCAL_R */
+	LABEL,  /* name (optional), address in LABEL_R */
 	/* actual opcodes */
 	ADD,    /* x := y op z  store result of binary operation on y and z to x */
 	SUB,
 	MUL,
 	DIV,
-	NEG,    /* x := op y      store result of unary operation on y to x */
-	ASN,    /* x := y store y to x */
-	ADDR,   /* x := &y        store address of y to x */
-	LCONT,  /* x := *y        store contents pointed to by y to x */
-	SCONT,  /* *x := y        store y to location pointed to by x */
-	GOTO,   /* goto L unconditional jump to L */
-	BLT,    /* if x rop y then goto L binary conditional jump to L */
+	NEG,    /* x := op y    store result of unary operation on y to x */
+	ASN,    /* x := y       store y to x */
+	ADDR,   /* x := &y      store address of y to x */
+	LCONT,  /* x := *y      store contents pointed to by y to x */
+	SCONT,  /* *x := y      store y to location pointed to by x */
+	GOTO,   /* goto L       unconditional jump to L */
+	BLT,    /* if x rop y then goto L  binary conditional jump to L */
 	BLE,
 	BGT,
 	BGE,
 	BEQ,
 	BNE,
-	BIF,    /* if x then goto L             unary conditional jump to L */
-	BNIF,   /* if !x then goto L              unary negative conditional jump to L */
+	BIF,    /* if x then goto L   unary conditional jump to L */
+	BNIF,   /* if !x then goto L  unary negative conditional jump to L */
 	PARM,   /* param x        store x as a parameter */
 	CALL,   /* call p, n, x   call procedure p with n parameters, store result in x */
 	RET     /* return x       return from procedure, use x as the result */
