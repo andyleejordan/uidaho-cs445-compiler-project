0df188f287491a5a0a090e28eb05ecfc77512238
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-12-06 22:26:04 -0800

    Updating fibonacci.cpp with for/while/do loops

--- data/pass/fibonacci.cpp
+++ data/pass/fibonacci.cpp
@@ -2,21 +2,28 @@ int fibonacci(int n);
 
 int main(int argc, char *argv[])
 {
-	int i = 2;
-	if (i == 2)
-		i = i * 2;
-	fibonacci(i % 8);
+	int c = 0;
+
+	while (c < 4) {
+		c = c + 2;
+	}
+
+	for (int i = 0; i < 32; ++i)
+		c++;
+
+	do {
+		fibonacci(c % 16);
+	} while (0);
 
 	return 0;
 }
 
 int fibonacci(int n)
 {
-	if (n == 0) {
+	if (n == 0)
 		return 0;
-	} else if (n == 1) {
+	else if (n == 1)
 		return 1;
-	} else {
+	else
 		return fibonacci(n - 1) + fibonacci(n - 2);
-	}
 }
