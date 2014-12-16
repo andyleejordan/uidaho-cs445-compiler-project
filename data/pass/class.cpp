6f8aa768ee87705cf9fa10c155f6ea5b47bc9ed6
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-12-16 01:40:06 -0800

    More test code in class.cpp

--- data/pass/class.cpp
+++ data/pass/class.cpp
@@ -1,20 +1,38 @@
 /* test code for classes */
 
+void nonmethod()
+{
+	return;
+}
+
 class TestClass {
 private:
 	int data;
 public:
 	TestClass();
 	int getData(int);
+	int moredata;
 };
 
 int main(int argc, char *argv[])
 {
+	nonmethod();
+
 	TestClass myclass;
 
-	TestClass *myclassptr = &myclass;
+	myclass.moredata = 2;
+	myclass.getData(1);
 
-	return myclassptr->getData(1);
+	TestClass *myaddress = &myclass;
+
+	TestClass *mynew = new TestClass();
+
+	mynew->moredata = 3;
+	mynew->getData(4);
+
+	delete mynew;
+
+	return 0;
 }
 
 TestClass::TestClass()
